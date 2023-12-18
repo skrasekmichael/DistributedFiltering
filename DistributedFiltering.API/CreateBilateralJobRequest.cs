27e14dbd1e4776757ddeacf65c502f820b21caeb
skrasek.michael <skrasek.michal@email.cz>
2023-12-18 23:18:54 +0100

    prepared api

---
+++ DistributedFiltering.API/CreateBilateralJobRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace DistributedFiltering.API;
+
+public sealed class CreateBilateralJobRequest
+{
+	public int UnitsCount { get; set; }
+	public double SpatialSigma { get; set; }
+	public double RangeSigma { get; set; }
+}
